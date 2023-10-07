d82ced24d69f67e5486fd502d62d70d963d67457
AAtlantizz <102814825+AAtlantizz@users.noreply.github.com>
2023-10-07 21:41:23 +0200

    limiter

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 });
 
 app.use(helmet());
-app.use(limiter);
+// app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
